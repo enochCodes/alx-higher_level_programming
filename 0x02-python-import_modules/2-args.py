af250d4ee7961c2862c6d4ee9fdfc33045183f2a
enochCodes <henoksamson57@gmail.com>
2024-03-20 22:09:16 +0300

    done

---
+++ 0x02-python-import_modules/2-args.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+from sys import argv
+
+def print_arg():
+    argc = len(argv) - 1
+    if argc == 0:
+        print("0 arguments.")
+        return
+    print("{} argument{}:".format(argc, "s" if argc > 1 else ""))
+    for i in range(1, len(argv)):
+        print("{}: {}".format(i, argv[i]))
+
+if __name__ == "__main__":
+    print_arg()
