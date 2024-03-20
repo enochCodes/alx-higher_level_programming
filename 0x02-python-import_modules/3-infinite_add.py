f401d80ad253bbeb7fba04a824219a7412166ab0
enochCodes <henoksamson57@gmail.com>
2024-03-21 00:08:30 +0300

    done

---
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+from sys import argv
+
+def infinite_add():
+    total = 0
+    for arg in argv[1:]:
+        total += int(arg)
+    print(total)
+
+if __name__ == "__main__":
+    infinite_add()
