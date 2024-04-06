57d7547a206b8ce7ea331cd3e3c5dea6714a8348
enochCodes <henoksamson57@gmail.com>
2024-04-06 14:48:34 +0300

    done

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""Defines a Square class."""
+
+
+class Square:
+    """Represents a square."""
+
+    def __init__(self, size):
+        """Initializes the square with the given size."""
+        self.__size = size
