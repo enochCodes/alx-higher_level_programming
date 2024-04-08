75047e221da9de846abc99d2b91063a2e1e76c91
enochCodes <enochcodes@enochcodes-hp-laptop.hi.link>
2024-04-09 01:05:50 +0300

    done

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -9,8 +9,8 @@ class Rectangle:
         return the high and the width
     """
     def __init__(self, width=0, height=0):
-        self.__width = width
-        self.__height = height
+        self.width = width
+        self.height = height
 
     @property
     def width(self):
@@ -20,9 +20,9 @@ class Rectangle:
     def width(self, value):
         if not isinstance(value, int):
             raise TypeError("width must be an integer")
-        elif value <= 0:
+        if value < 0:
             raise ValueError("width must be >= 0")
-        return self.width
+        self.__width = value
 
     @property
     def height(self):
@@ -32,5 +32,6 @@ class Rectangle:
     def height(self, value):
         if not isinstance(value, int):
             raise TypeError("height must be an integer")
-        elif value <= 0:
+        if value < 0:
             raise ValueError("hegiht must be >= 0")
+        self.__height = value
