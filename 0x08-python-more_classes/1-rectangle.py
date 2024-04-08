38e2ea2b556712f3ab9392cf49117f72bb52bb2a
enochCodes <enochcodes@enochcodes-hp-laptop.hi.link>
2024-04-09 01:00:46 +0300

    done

---
+++ 0x08-python-more_classes/1-rectangle.py
@@ -0,0 +1,36 @@
+#!/usr/bin/python3
+"""
+    0-rectangle: class Rectangle
+"""
+
+
+class Rectangle:
+    """
+        return the high and the width
+    """
+    def __init__(self, width=0, height=0):
+        self.__width = width
+        self.__height = height
+
+    @property
+    def width(self):
+        return self.__width
+
+    @width.setter
+    def width(self, value):
+        if not isinstance(value, int):
+            raise TypeError("width must be an integer")
+        elif value <= 0:
+            raise ValueError("width must be >= 0")
+        return self.width
+
+    @property
+    def height(self):
+        return self.__height
+
+    @height.setter
+    def height(self, value):
+        if not isinstance(value, int):
+            raise TypeError("height must be an integer")
+        elif value <= 0:
+            raise ValueError("hegiht must be >= 0")
