b98e1814a6d0e82885ae3f22562637713a73dc4f
enochCodes <henoksamson57@gmail.com>
2024-04-15 19:33:27 +0300

    done

--- 0x0A-python-inheritance/1-my_list.py
+++ 0x0A-python-inheritance/1-my_list.py
@@ -4,8 +4,7 @@
 
 
 class MyList(list):
-    """create mylist with interts of list"""
+    ''' This class inherits from list() '''
     def print_sorted(self):
         """Print the list sorted in ascending order."""
-        sorted_list = sorted(self)
-        print(sorted_list)
+        print(sorted(self))
