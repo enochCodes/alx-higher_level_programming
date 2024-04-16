34f3b76194c6c15f5ea8c824b748d608928a42b1
enochCodes <henoksamson57@gmail.com>
2024-04-16 03:10:03 +0300

    code

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""5-base_geometry.py
+"""
+
+
+class BaseGeometry:
+    """empty class"""
+    def area(self):
+        """raise the Exception"""
+        raise Exception("area() is not implemented")
