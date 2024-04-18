3356f4c54bc5c733827e4195f91d59098f0739e5
enochCodes <henoksamson57@gmail.com>
2024-04-18 23:03:04 +0300

    done

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -14,12 +14,9 @@ def add_integer(a, b=98):
         Sum of the two arguments
     Raises:
         TypeError: If either of the arguments not an integer or a float
-"""
-
-
-if not isinstance(a, (int, float)):
-    raise TypeError("a must be an integer")
-if not isinstance(b, (int, float)):
-    raise TypeError("b must be an integer")
-
+    """
+    if not isinstance(a, (int, float)):
+        raise TypeError("a must be an integer")
+    if not isinstance(b, (int, float)):
+        raise TypeError("b must be an integer")
     return int(a) + int(b)
