d934af2aea05a17f6354f0fc7a870c822370fa30
enochCodes <henoksamson57@gmail.com>
2024-05-23 03:40:33 +0300

    createdone

--- 0x0D-SQL_introduction/1-create_database_if_missing.sql
+++ 0x0D-SQL_introduction/1-create_database_if_missing.sql
@@ -1,6 +1,11 @@
 -- File: create_database.sql
--- Description: This script creates a new database named 'my_new_database'
+-- Description: This script creates a new database named 'my_new_database' if it does not already exist.
 
--- Create a new database
-CREATE DATABASE my_new_database;
+-- Check if the database already exists
+SELECT SCHEMA_NAME
+FROM INFORMATION_SCHEMA.SCHEMATA
+WHERE SCHEMA_NAME = 'hbtn_0c_0';
+
+-- If the database does not exist, createt
+CREATE DATABASE IF NOT EXISTS my_new_database;
 
