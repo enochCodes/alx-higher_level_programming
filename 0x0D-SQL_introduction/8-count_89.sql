26f135f099e6a21b6db5bd3356b981e0ac27142b
enochCodes <henoksamson57@gmail.com>
2024-05-23 18:23:34 +0300

    done

---
+++ 0x0D-SQL_introduction/8-count_89.sql
@@ -0,0 +1,6 @@
+-- File: 8-count_89.sql
+-- Description: This script displays the number of records with id = 89 in the table first_table of the database hbtn_0c_0.
+
+-- Count the number of records with id = 89
+SELECT COUNT(*) FROM first_table WHERE id = 89;
+
