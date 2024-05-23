90f076bc8baf9d2acaca819587517dde00b3613f
enochCodes <henoksamson57@gmail.com>
2024-05-23 03:44:57 +0300

    done

--- 0x0D-SQL_introduction/1-create_database_if_missing.sql
+++ 0x0D-SQL_introduction/1-create_database_if_missing.sql
@@ -4,7 +4,3 @@
 -- Attempt to create the database if it does not exist
 CREATE DATABASE IF NOT EXISTS my_new_database;
 
--- Optionally, you can verify the creation by listing all databases
--- Note: This is optional and depends on project requirements
--- SHOW DATABASES;
-
