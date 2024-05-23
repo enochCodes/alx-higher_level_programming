4db8c739a1b99219219956014883acc2f9953e2b
enochCodes <henoksamson57@gmail.com>
2024-05-23 18:00:38 +0300

    done

--- 0x0D-SQL_introduction/5-full_table.sql
+++ 0x0D-SQL_introduction/5-full_table.sql
@@ -1,12 +1,6 @@
 -- File: 5-full_table.sql
 -- Description: This script prints the full description of the table first_table from the database hbtn_0c_0.
 
--- Select the full table creation statement from information_schema
-
-
--- Select column information from information_schema
-SELECT * FROM information_schema.tables
-WHERE table_schema = 'hbtn_0c_0' AND table_name = 'first_table';
-SELECT * FROM information_schema.columns
-WHERE table_schema = 'hbtn_0c_0' AND table_name = 'first_table';
+-- Get the CREATE TABLE statement for first_table
+SHOW CREATE TABLE first_table;
 
