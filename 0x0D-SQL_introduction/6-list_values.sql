02981108e077ee54b7341265e00571eb75e4a63c
enochCodes <henoksamson57@gmail.com>
2024-05-23 18:02:15 +0300

    done

---
+++ 0x0D-SQL_introduction/6-list_values.sql
@@ -0,0 +1,6 @@
+-- File: 6-list_values.sql
+-- Description: This script lists all rows of the table first_table from the database hbtn_0c_0.
+
+-- Select all rows from first_table
+SELECT * FROM first_table;
+
