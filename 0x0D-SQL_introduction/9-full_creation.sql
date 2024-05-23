7e0d8d45126cc2446af6b771103e954c33535de2
enochCodes <henoksamson57@gmail.com>
2024-05-23 18:34:51 +0300

    done

--- 0x0D-SQL_introduction/9-full_creation.sql
+++ 0x0D-SQL_introduction/9-full_creation.sql
@@ -6,7 +6,7 @@ CREATE TABLE IF NOT EXISTS second_table (
 	name VARCHAR(256),
 	score INT
 );
-INSERT INTO second_table (id, name, score) VALUES (1, 'John', score = 10)
-INSERT INTO second_table (id, name, score) VALUES (2, 'Alex', score = 3)
-INSERT INTO second_table (id, name, score) VALUES (3, 'Bob', score = 14)
-INSERT INTO second_table (id, name, score) VALUES (4, 'George', score = 8)
+INSERT INTO second_table (id, name, score) VALUES (1, 'John', 10)
+INSERT INTO second_table (id, name, score) VALUES (2, 'Alex', 3)
+INSERT INTO second_table (id, name, score) VALUES (3, 'Bob', 14)
+INSERT INTO second_table (id, name, score) VALUES (4, 'George', 8)
