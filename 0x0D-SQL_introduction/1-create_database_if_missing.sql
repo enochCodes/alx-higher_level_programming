c4f063bb8fe43b527c83b1d1584f9595169c64e3
enochCodes <henoksamson57@gmail.com>
2024-05-23 03:46:16 +0300

    done

--- 0x0D-SQL_introduction/1-create_database_if_missing.sql
+++ 0x0D-SQL_introduction/1-create_database_if_missing.sql
@@ -2,5 +2,5 @@
 -- Description: This script attempts to create a new database named 'my_new_database' if it does not already exist.
 
 -- Attempt to create the database if it does not exist
-CREATE DATABASE IF NOT EXISTS my_new_database;
+CREATE DATABASE IF NOT EXISTS hbtn_0c_0;
 
