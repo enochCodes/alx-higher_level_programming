b1884f2f387ef291266db04b8370d91e2d12f0bc
enochCodes <henoksamson57@gmail.com>
2024-05-23 18:56:59 +0300

    done

--- 0x0D-SQL_introduction/11-best_score.sql
+++ 0x0D-SQL_introduction/11-best_score.sql
@@ -1,6 +1,6 @@
--- File: 10-top_score.sql
+-- File: 11-best_score.sql
 -- Description: This script lists all records of the table second_table of the database hbtn_0c_0.
--- The results display both the score and the name in this order, ordered by score (top first).
+-- The results display both the score and the name in this order, ordered by 10 < score (top first).
 
 -- Select score and name from second_table, ordered by score descending
 SELECT score, name
