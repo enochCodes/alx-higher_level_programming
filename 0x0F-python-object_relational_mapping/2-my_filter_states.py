f4ae85c62940ffe929bd3226dd9c6dbce5c34c80
enochCodes <henoksamson57@gmail.com>
2024-06-25 02:57:53 +0300

    done

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -1,30 +1,26 @@
 #!/usr/bin/python3
-"""
-    database connect feach the result wtih N serch
-"""
 import MySQLdb
 import sys
 
 if __name__ == "__main__":
-
     mysql_username = sys.argv[1]
     mysql_password = sys.argv[2]
     db_name = sys.argv[3]
     state_name = sys.argv[4]
 
     db = MySQLdb.connect(
-            host="localhost",
-            port=3306,
-            user=mysql_username,
-            password=mysql_password,
-            db=db_name
-            )
+        host="localhost",
+        port=3306,
+        user=mysql_username,
+        passwd=mysql_password,
+        db=db_name
+    )
 
     cursor = db.cursor()
-    sql_query = (
-        "SELECT * FROM states WHERE name = %s ORDER BY id ASC"
-        )
-    cursor.execute(sql_query, (state_name,))
+
+    # Use parameterized query to avoid SQL injection
+    query = "SELECT * FROM states WHERE name = %s ORDER BY id ASC"
+    cursor.execute(query, (state_name,))
 
     rows = cursor.fetchall()
 
