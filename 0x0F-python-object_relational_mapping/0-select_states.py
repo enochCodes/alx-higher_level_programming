7cc7633ec051fde44197d2531f307705728c3d4b
enochCodes <henoksamson57@gmail.com>
2024-06-25 01:18:47 +0300

    done

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -1,13 +1,18 @@
 #!/usr/bin/python3
+"""
+    database connect feach the result
+"""
 import MySQLdb
 import sys
 
 if __name__ == "__main__":
 
+    #get the database name, password and database host form command line arguments
     mysql_username = sys.argv[1]
     mysql_password = sys.argv[2]
     db_name = sys.argv[3]
 
+    #connect to the MYSQL server
     db = MySQLdb.connect(
             host="localhost",
             port=3306,
