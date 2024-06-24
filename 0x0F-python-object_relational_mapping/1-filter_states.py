9963084429be521c240ba0d1740586ccfa4de1e0
enochCodes <henoksamson57@gmail.com>
2024-06-25 01:42:20 +0300

    done

---
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python3
+"""
+    database connect feach the result wtih N
+"""
+import MySQLdb
+import sys
+
+if __name__ == "__main__":
+
+    mysql_username = sys.argv[1]
+    mysql_password = sys.argv[2]
+    db_name = sys.argv[3]
+
+    db = MySQLdb.connect(
+            host="localhost",
+            port=3306,
+            user=mysql_username,
+            password=mysql_password,
+            db=db_name
+            )
+
+    cursor = db.cursor()
+
+    cursor.execute("SELECT *FROM states WHERE name LIKE 'N%'")
+
+    rows = cursor.fetchall()
+
+    for row in rows:
+        print(row)
+
+    cursor.close()
+    db.close()
