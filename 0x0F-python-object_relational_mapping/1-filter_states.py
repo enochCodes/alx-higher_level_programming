c7e8b97f5ce8864cb2be9625b1ac00cdd0416dcf
enochCodes <henoksamson57@gmail.com>
2024-06-25 03:02:29 +0300

    done

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -1,29 +1,28 @@
 #!/usr/bin/python3
 """
-    database connect feach the result wtih N
+    Databse connect 1
 """
 import MySQLdb
 import sys
 
 if __name__ == "__main__":
-
     mysql_username = sys.argv[1]
     mysql_password = sys.argv[2]
     db_name = sys.argv[3]
 
+    # Connect to the MySQL server
     db = MySQLdb.connect(
-            host="localhost",
-            port=3306,
-            user=mysql_username,
-            password=mysql_password,
-            db=db_name
-            )
-
+        host="localhost",
+        port=3306,
+        user=mysql_username,
+        passwd=mysql_password,
+        db=db_name
+    )
     cursor = db.cursor()
 
-    cursor.execute(
-            "SELECT *FROM states WHERE name LIKE  BINARY 'N%' ORDER BY id ASC"
-            )
+    query = "SELECT * FROM states WHERE name LIKE BINARY 'N%' ORDER BY id ASC"
+
+    cursor.execute(query)
 
     rows = cursor.fetchall()
 
