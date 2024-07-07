046df848a445bf9db3dee5e4624231a76b52fd8a
enochCodes <henoksamson57@gmail.com>
2024-07-07 23:55:24 +0300

    done

--- 0x10-python-network_0/1-body.sh
+++ 0x10-python-network_0/1-body.sh
@@ -1,9 +1,3 @@
 #!/bin/bash
 # Sends a GET request to a given URL and displays the body of the response if the status code is 200
-
-URL=$1
-
-response=$(curl -s -w "%{http_code}" -o response_body.txt "$URL")
-if [ "$response" -eq 200 ]; then
-  cat response_body.txt
-fi
+curl -sL -w "%{http_code}" "$1" -o /dev/null | grep -q "200" && curl -sL "$1"
