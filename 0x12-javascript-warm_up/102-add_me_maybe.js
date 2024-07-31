e048f57a52786daa0788733330bd113317d4fda7
enochCodes <henoksamson57@gmail.com>
2024-08-01 00:11:34 +0300

    done

---
+++ 0x12-javascript-warm_up/102-add_me_maybe.js
@@ -0,0 +1,9 @@
+#!/usr/bin/node
+
+exports.addMeMaybe = function (number, theFunction) {
+  // Increment the number
+  const incrementedNumber = number + 1;
+
+  // Call the provided function with the incremented number
+  theFunction(incrementedNumber);
+};
