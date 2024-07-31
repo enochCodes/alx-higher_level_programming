a5937dfa8cb1d3e508fdd6be978a35bfc12acd7a
enochCodes <henoksamson57@gmail.com>
2024-07-31 22:37:56 +0300

    done_8

--- 0x12-javascript-warm_up/8-square.js
+++ 0x12-javascript-warm_up/8-square.js
@@ -1,11 +1,17 @@
 #!/usr/bin/node
 
+// Get the first argument passed to the script
 const arg = process.argv[2];
+
+// Convert the argument to an integer
 const size = parseInt(arg);
-if (isNaN(size)) {
-	console.log('Missing size');
+
+// Check if the conversion was successful and size is a positive integer
+if (isNaN(size) || size <= 0) {
+  console.log('Missing size');
 } else {
-	for (let i = 0; i < size; i++) {
-		console.log('X'.repeat(size));
-	}
+  // Print the square of 'X' characters
+  for (let i = 0; i < size; i++) {
+    console.log('X'.repeat(size));
+  }
 }
