1a254807b807426154fc51d4512ad0a3a9bcf190
enochCodes <henoksamson57@gmail.com>
2024-07-31 14:03:43 +0300

    done7

---
+++ 0x12-javascript-warm_up/7-multi_c.js
@@ -0,0 +1,15 @@
+#!/usr/bin/node
+
+// Get the first argument passed to the script
+const arg = process.argv[2];
+// Convert the argument to an integer
+const x = parseInt(arg);
+// Check if the conversion was successful and x is a positive integer
+if (isNaN(x) || x < 0) {
+  console.log('Missing number of occurrences');
+} else {
+  // Print "C is fun" x times
+  for (let i = 0; i < x; i++) {
+    console.log('C is fun');
+  }
+}
