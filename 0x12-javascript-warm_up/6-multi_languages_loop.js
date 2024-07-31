b9a86fafa6ced7aed3e4ce3e08b6b8c29a71d968
enochCodes <henoksamson57@gmail.com>
2024-07-31 13:56:25 +0300

    doen

--- 0x12-javascript-warm_up/6-multi_languages_loop.js
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -2,7 +2,6 @@
 
 // Array of strings to print
 const Languages = ["C is fun","Python is cool","JavaScript is amazing"];
-
 // Loop through the array and print each string
 for (const language of Languages) {
   console.log(language);
