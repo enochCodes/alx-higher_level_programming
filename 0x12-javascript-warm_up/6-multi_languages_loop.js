0efb34785a4ce0b494f9b1fdc6f90d60a2dcd6bd
enochCodes <henoksamson57@gmail.com>
2024-07-31 13:44:31 +0300

    done

--- 0x12-javascript-warm_up/6-multi_languages_loop.js
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -2,6 +2,6 @@
 
 const Languages = ["C is fun","Python is cool","JavaScript is amazing"];
 
-for (let i = 0; i <= Languages.length; i++) {
-	console.log(Languages[i]);
+for (const language of Languages) {
+  console.log(language);
 }
