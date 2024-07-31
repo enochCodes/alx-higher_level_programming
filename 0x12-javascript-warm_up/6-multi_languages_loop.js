6e84c96d79a7fe1ddebbe2960ea2bc8ba158d7d8
enochCodes <henoksamson57@gmail.com>
2024-07-31 13:53:55 +0300

    fix

--- 0x12-javascript-warm_up/6-multi_languages_loop.js
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -1,7 +1,10 @@
 #!/usr/bin/node
 
+
+// Array of strings to print
 const Languages = ["C is fun","Python is cool","JavaScript is amazing"];
 
+// Loop through the array and print each string
 for (const language of Languages) {
   console.log(language);
 }
