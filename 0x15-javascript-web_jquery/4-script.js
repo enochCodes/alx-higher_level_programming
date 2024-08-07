0bb7e127a2cd0530faa9e0d6f19453c1869ac6df
enochCodes <henoksamson57@gmail.com>
2024-08-07 22:33:27 +0300

    style(add-toggle): add togle for heade

---
+++ 0x15-javascript-web_jquery/4-script.js
@@ -0,0 +1,11 @@
+$(function(){
+	$('#toggle_header').click(function() {
+		const header = $('header');
+
+		if (header.hasClass('red')) {
+			header.removeClass('red').addclass('green')
+		} else {
+			header.removeClass('green').addClass('red');
+		}
+	});
+});
