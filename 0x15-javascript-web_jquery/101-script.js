f5cfd6dfd6ccefdfe347b844eec7a4f0d6673dca
enochCodes <henoksamson57@gmail.com>
2024-08-08 00:22:07 +0300

    refactor(aadd): add

---
+++ 0x15-javascript-web_jquery/101-script.js
@@ -0,0 +1,12 @@
+$(function() {
+	$('#add_item').click(function() {
+		$('.my_list').append('<li>Item</li>');
+	});
+
+	$('#remove_item').click(function() {
+		$('.my_list li:last-child').remove();
+	});
+	$('#clear_list').click(function() {
+		$('.my_list').empty();
+	});
+})
