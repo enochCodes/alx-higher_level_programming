352b33f5bc82022e887ad0f81d5a97f33e1110b3
enochCodes <henoksamson57@gmail.com>
2024-08-07 23:03:41 +0300

    feat(feath-test-API): add

---
+++ 0x15-javascript-web_jquery/7-script.js
@@ -0,0 +1,9 @@
+$(function() {
+	const url: 'https://swapi-api.alx-tools.com/api/people/5/?format=json';
+	
+	$.get(url, function(data){
+		const characterName = data.name;
+
+		$('#character').text(characterName);
+	});
+});
